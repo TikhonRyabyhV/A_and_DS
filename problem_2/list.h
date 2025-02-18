60e368540872ca615cb1cdf1d79d492191e63454
Tikhon Ryabykh <riabykh.tv@phystech.edu>
2025-02-18 18:03:29 +0300

    Created small library (list.h, list.c) for the second task

---
+++ problem_2/list.h
@@ -0,0 +1,30 @@
+#pragma once
+
+typedef struct list_member {
+	
+	char* str ;
+	int   size;
+
+	struct list_member* prev_member;
+	struct list_member* next_member;
+
+} list_member_st;
+
+typedef struct list_head {
+	
+	int list_size;
+
+	list_member_st* first_member;
+	list_member_st* last_member ;
+
+
+} list_head_st;	
+
+
+list_member_st* goto_member (list_head_st* src, int place);
+
+void insert_member (list_head_st* src, int range, char* str, int size);
+
+void delete_member (list_head_st* src, int place);
+
+void clear_list (list_head_st* src);
